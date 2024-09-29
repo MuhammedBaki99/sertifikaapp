4329007cd8e39096aa67e19174d20b73e6cc6f8f
Muhammed Baki <muhammedbakicaglayan@gmail.com>
2024-09-29 13:36:28 +0300

    asdasd

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return ( 
-    <h2>ben kemal</h2>
+  return (
+    <>
+      <h2>ben kemal</h2>
+      <h2>ben muhammed</h2>
+    </>
   );
 }
